fd611d9b10b6a2759a35d8be838b9b7bdf95a9c5
jhagg26 <jhagg26@gmail.com>
2023-05-04 20:44:12 -0400

    Add math operations

---
+++ script.js
@@ -0,0 +1,6 @@
+// CALCULATOR FUNCTIONS 
+function add(a,b) { return a+b; }
+function subtract(a,b) { return a-b; }
+function multiply(a,b) { return a*b; }
+function divide(a,b) { return a/b; }
+function modulus(a,b) { return a%b; }
