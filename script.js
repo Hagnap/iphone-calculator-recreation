fa380680bd41859dc0af40b62459adf6fe09a40a
jhagg26 <jhagg26@gmail.com>
2023-05-04 20:57:17 -0400

    Add interactive buttons and log output to console

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ function subtract(a,b) { return a-b; }
 function multiply(a,b) { return a*b; }
 function divide(a,b) { return a/b; }
 function modulus(a,b) { return a%b; }
+
+// DOM FUNCTIONS
+const numberBtns = document.querySelectorAll(".number");
+numberBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.textContent);
+    })
+});
+
+const addBtn = document.querySelector("#add");
+addBtn.addEventListener("click", () => {
+    console.log("+");
+});
+
+const subtractBtn = document.querySelector("#subtract");
+subtractBtn.addEventListener("click", () => {
+    console.log("-");
+});
+
+const multiplyBtn = document.querySelector("#multiply");
+multiplyBtn.addEventListener("click", () => {
+    console.log("*");
+});
+
+const divideBtn = document.querySelector("#divide");
+divideBtn.addEventListener("click", () => {
+    console.log("/");
+});
+
+const modulusBtn = document.querySelector("#modulus");
+modulusBtn.addEventListener("click", () => {
+    console.log("%");
+});
+
+const decimalBtn = document.querySelector("#decimal");
+decimalBtn.addEventListener("click", () => {
+    console.log(".");
+});
+
+const equalBtn = document.querySelector("#equal");
+equalBtn.addEventListener("click", () => {
+    console.log("=");
+});
