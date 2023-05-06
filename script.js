285e3704b552659519b7122bd6688b3d6c83a1c6
jhagg26 <jhagg26@gmail.com>
2023-05-05 21:58:42 -0400

    Add operate code and equal button code

--- script.js
+++ script.js
@@ -5,6 +5,47 @@ function multiply(a,b) { return a*b; }
 function divide(a,b) { return a/b; }
 function modulus(a,b) { return a%b; }
 
+function clear() {
+    userInput = "0";
+    document.querySelector("#output-text").textContent = userInput;
+}
+
+function operate(operator, a, b) {
+    if(a === "" || b == "") {
+        return "ERROR: MAKE SURE USER INPUT IS VALID!";
+    }
+    
+    switch(operator){
+        case "+":
+            return add(a,b);
+            break;
+
+        case "-":
+            return subtract(a,b);
+            break;
+
+        case "*":
+            return multiply(a,b);
+            break;
+
+        case "/":
+
+            if(b === 0) {
+                return "ERROR: CANT NOT DIVIDE BY ZERO!";
+            }
+            else{
+                return divide(a,b);
+            }
+            
+            break;
+
+        case "%":
+
+            return modulus(a,b);
+            break;
+    }
+}
+
 // DOM FUNCTIONS
 const numberBtns = document.querySelectorAll(".number");
 numberBtns.forEach((button) => {
@@ -43,25 +84,44 @@ decimalBtn.addEventListener("click", () => {
     updateUserInput(".");
 });
 
-const equalBtn = document.querySelector("#equal");
-equalBtn.addEventListener("click", () => {
-    console.log("=");
-});
-
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", () => {
     clear();
 })
 
-// Input Functions
+const equalBtn = document.querySelector("#equal");
+equalBtn.addEventListener("click", () => {
+    
+    // RegEx = https://masteringjs.io/tutorials/fundamentals/split-on-multiple-characters
+    let result = userInput.split(/[*/+-]+/);
+
+    let operator = "";
+    let a = "";
+    let b = "";
+
+    if(userInput[0] === "-") {
+        operator = userInput[result[1].length+1];
+        a = Number(result[1]);
+        a *= -1;
+        b = Number(result[2]);
+    }
+
+    else {
+        operator = userInput[result[0].length];
+        a = Number(result[0]);
+        b = Number(result[1]);
+    }
+
+    userInput = operate(operator, a, b);
+    
+    document.querySelector("#output-text").textContent = userInput;
+
+});
+
+// Input Function
 function updateUserInput(value){
     //if(value == "0") { userInput = value; }
     userInput = userInput == "0" ? value : userInput += value;
-    document.querySelector("#output-text").textContent = userInput;
-}
-
-function clear() {
-    userInput = "0";
     document.querySelector("#output-text").textContent = userInput;
 }
 
